3bf944954f8896351602c8bb58c425ead4498069
Fabien Czimmernings <fabi@czimmernings.de>
2023-01-12 08:11:31 +0100

    refactor

--- my-vue-app/src/components/ProductList.vue
+++ my-vue-app/src/components/ProductList.vue
@@ -9,15 +9,18 @@ defineProps(['products'])
   <div class="productList">
     <div class="productList__item" v-for="item in products" >
       <div class="productList__item-title bold">
-        Lebewesen: <span class="content">{{ item.title }}</span>
+        Lebewesen: 
+        <span class="content">
+          {{ item.title }}
+        </span>
       </div>
       <div class="productList__item-legs bold">
-        Anzahl Beine: <span class="content">{{ item.legs }}</span>
+        Anzahl Beine: 
+        <span class="content">
+          {{ item.legs }}
+        </span>
       </div>
     </div>
   </div>
 </template>
 
-<style lang="scss">
-
-</style>
